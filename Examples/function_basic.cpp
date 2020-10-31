9e01029b127cc21d2e498c4a0f5bd0f5b3c6cfbe
hassan84226 <71786895+hassan84226@users.noreply.github.com>
2020-10-31 15:36:19 -0400

    .

---
+++ Examples/function_basic.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+//returntype name(parameter)
+//part 1
+void printmessage(void);
+int main(){
+
+printmessage();
+printmessage();
+
+return 0;
+}
+//part 3
+//function implementation
+void printmessage(void){
+cout<<"You are at this function\n";
+
+}
