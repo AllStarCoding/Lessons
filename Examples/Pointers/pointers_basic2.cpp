e0743066add5d708a6f1a0fbb54e22bc6f9ebd2e
hassan84226 <71786895+hassan84226@users.noreply.github.com>
2020-10-31 15:43:29 -0400

    Create pointers_basic2.cpp

---
+++ Examples/Pointers/pointers_basic2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    float i, *p=&i;
+    int *q;
+
+
+    cout<<i<<endl;
+
+    cout<<*q<<endl;
+    cout<<*p<<endl;
+    cout<<*q<<endl;
+    return 0;
+}
